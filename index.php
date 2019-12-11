2179c9718733d784fd19ded153a704365a11235e
Iz-Dine Amadou <2088346@talnet.nl>
2019-12-11 10:11:04 +0100

    Uitwerking van de exercise

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+<h1>
+<?php
+ echo "ik ben izdine en zit bij Bit academy ";
+
+ ?></h1>
+  </body>
+</html>
